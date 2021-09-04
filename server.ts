cd9bb41663503fb5fa064ae702b5f97910e2b029
kawarimidoll <kawarimidoll+git@gmail.com>
2021-09-05 08:10:22 +0900

    chore(example): fix server.ts for Deno Deploy Beta 2

--- server.ts
+++ server.ts
@@ -10,8 +10,18 @@ const log = new Log({
   addNewLine: true,
 });
 
-addEventListener("fetch", (event) => {
-  const { host, pathname, searchParams } = new URL(event.request.url);
+const listener = Deno.listen({ port: 8080 });
+console.log(`HTTP server listening on http://localhost:${listener.addr.port}`);
+
+async function handleConn(conn: Deno.Conn) {
+  const httpConn = Deno.serveHttp(conn);
+  for await (const e of httpConn) {
+    e.respondWith(handler(e.request, conn));
+  }
+}
+
+function handler(request: Request, _conn: Deno.Conn) {
+  const { host, pathname, searchParams } = new URL(request.url);
   const params = Object.fromEntries([...searchParams.entries()]);
 
   log.debug({ host });
@@ -20,21 +30,35 @@ addEventListener("fetch", (event) => {
   if (pathname === "/error") {
     log.error("error!");
 
-    event.respondWith(new Response("error page!", { status: 500 }));
-    return;
+    return new Response("error page!", { status: 500 });
   }
-
-  const message = "Hello Deno Deploy! from " + pathname;
 
   if (pathname.endsWith(".json")) {
     log.warn("JSON API is called");
-    event.respondWith(
-      new Response(JSON.stringify({ message, params }), {
-        headers: { "content-type": "application/json" },
-      }),
-    );
-    return;
+    return new Response(JSON.stringify({ pathname, params }), {
+      headers: { "content-type": "application/json" },
+    });
   }
 
-  event.respondWith(new Response(message));
-});
+  return new Response(
+    `<!DOCTYPE html><html><head><title>tl-log example</title></head>
+    <body>
+      <div>Hello from ${pathname}.</div>
+      <div>
+        <p>Try to access these links to show the logs.</p>
+        <ul>
+          <li><a href="/test">/test</a></li>
+          <li><a href="/test.json">/test.json</a></li>
+          <li><a href="/error">/error</a></li>
+        </ul>
+      </div>
+    </body></html>`,
+    {
+      headers: { "content-type": "text/html" },
+    },
+  );
+}
+
+for await (const conn of listener) {
+  handleConn(conn);
+}
