61d11183da03822f6f027aa06fba29ce2b81cfe7
kawarimidoll <kawarimidoll+git@gmail.com>
2021-09-04 16:03:38 +0900

    test: improve test

--- test.ts
+++ test.ts
@@ -1,15 +1,16 @@
 import { assertEquals } from "./deps.ts";
 import { Log, LogLevel } from "./mod.ts";
 
+const levels = ["debug", "info", "warn", "error"] as LogLevel[];
+
 const date = new Date(2020, 9, 6, 8, 2, 45);
-function generateLogs(log: Log) {
-  const levels = ["debug", "info", "warn", "error"] as LogLevel[];
+function generateLogPrefixes(log: Log) {
   return levels.map((level) => log.prefix(date, level));
 }
 
 Deno.test("normal", () => {
   assertEquals(
-    generateLogs(new Log()),
+    generateLogPrefixes(new Log()),
     [
       "\x1b[0m2020-10-06T08:02:45+09:00 ✔\x1b[0m",
       "\x1b[34m2020-10-06T08:02:45+09:00 ℹ\x1b[39m",
@@ -21,7 +22,7 @@ Deno.test("normal", () => {
 
 Deno.test("no levels", () => {
   assertEquals(
-    generateLogs(new Log({ levelIndicator: "none" })),
+    generateLogPrefixes(new Log({ levelIndicator: "none" })),
     [
       "\x1b[0m2020-10-06T08:02:45+09:00\x1b[0m",
       "\x1b[34m2020-10-06T08:02:45+09:00\x1b[39m",
@@ -33,7 +34,7 @@ Deno.test("no levels", () => {
 
 Deno.test("initial", () => {
   assertEquals(
-    generateLogs(new Log({ levelIndicator: "initial" })),
+    generateLogPrefixes(new Log({ levelIndicator: "initial" })),
     [
       "\x1b[0m2020-10-06T08:02:45+09:00 D\x1b[0m",
       "\x1b[34m2020-10-06T08:02:45+09:00 I\x1b[39m",
@@ -45,7 +46,7 @@ Deno.test("initial", () => {
 
 Deno.test("full", () => {
   assertEquals(
-    generateLogs(new Log({ levelIndicator: "full" })),
+    generateLogPrefixes(new Log({ levelIndicator: "full" })),
     [
       "\x1b[0m2020-10-06T08:02:45+09:00 DEBUG\x1b[0m",
       "\x1b[34m2020-10-06T08:02:45+09:00 INFO \x1b[39m",
@@ -57,7 +58,7 @@ Deno.test("full", () => {
 
 Deno.test("change datetime format", () => {
   assertEquals(
-    generateLogs(new Log({ datetimeFormat: "MMM, d, YYYY, h:m a" })),
+    generateLogPrefixes(new Log({ datetimeFormat: "MMM, d, YYYY, h:m a" })),
     [
       "\x1b[0mOct, 6, 2020, 8:2 AM ✔\x1b[0m",
       "\x1b[34mOct, 6, 2020, 8:2 AM ℹ\x1b[39m",
@@ -69,12 +70,46 @@ Deno.test("change datetime format", () => {
 
 Deno.test("no timestamp", () => {
   assertEquals(
-    generateLogs(new Log({ datetimeFormat: "" })),
+    generateLogPrefixes(new Log({ datetimeFormat: "" })),
     [
       "\x1b[0m✔\x1b[0m",
       "\x1b[34mℹ\x1b[39m",
       "\x1b[33m⚠\x1b[39m",
       "\x1b[31m✖\x1b[39m",
+    ],
+  );
+});
+
+function collectConsoleArgs(log: Log) {
+  const result: unknown[] = [];
+  levels.forEach((level) => {
+    // stub console
+    console[level] = (...args: unknown[]) => {
+      result.push(args);
+    };
+    log[level](level);
+  });
+  return result;
+}
+
+Deno.test("add newline", () => {
+  assertEquals(
+    collectConsoleArgs(new Log({ datetimeFormat: "", addNewLine: true })),
+    [
+      ["\x1b[0m✔\x1b[0m", "debug", "\n"],
+      ["\x1b[34mℹ\x1b[39m", "info", "\n"],
+      ["\x1b[33m⚠\x1b[39m", "warn", "\n"],
+      ["\x1b[31m✖\x1b[39m", "error", "\n"],
+    ],
+  );
+});
+
+Deno.test("minimum level", () => {
+  assertEquals(
+    collectConsoleArgs(new Log({ datetimeFormat: "", minLogLevel: "warn" })),
+    [
+      ["\x1b[33m⚠\x1b[39m", "warn"],
+      ["\x1b[31m✖\x1b[39m", "error"],
     ],
   );
 });
