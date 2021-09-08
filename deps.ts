4067f46a957f109534ff9869f96274b74e8cbd12
kawarimidoll <kawarimidoll+git@gmail.com>
2021-09-08 10:20:33 +0900

    chore(deps): update dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { assertEquals } from "https://deno.land/std@0.101.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
 
 export {
   blue,
