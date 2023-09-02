02e0ff72b28080e91b4e4848badd20d5222f0cf5
GitHub <noreply@github.com>
2023-09-02 00:49:33 +0000

    chore(deps): update deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
-export { assertEquals } from "https://deno.land/std@0.200.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.201.0/testing/asserts.ts";
 
 export {
   blue,
   red,
   reset,
   yellow,
-} from "https://deno.land/std@0.200.0/fmt/colors.ts";
+} from "https://deno.land/std@0.201.0/fmt/colors.ts";
 
 export { datetime } from "https://deno.land/x/ptera@v1.0.2/datetime.ts";
